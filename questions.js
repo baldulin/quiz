04aa67ea3737bcb67f081d6e38c9625f76394448
Michael Schmidt <michael.schmidt@schmidt.tw>
2014-09-04 21:28:05 +0200

    More questions

--- questions.js
+++ questions.js
@@ -10,5 +10,17 @@ data = [
 "answers": [ "Berlin", "Paris", "Brussels", "Warsaw" ],
 "solution": 4,
 "text": "Brussels is the biggest city in belgium and its captial."
+},
+{
+"question": "What is the distance from earth to moon?",
+"answers": [ "384.400 km", "900.500 km", "2.500 km", "80.000 km" ],
+"solution": 1,
+"text": "So far and still so close."
+},
+{
+"question": "Who was not member of Monty Python?",
+"answers": [ "John Cleese", "Terry Gilliam", "Eric Idle", "Elton John" ],
+"solution": 4,
+"text": "Obviously Elton John was not part of Monty Python."
 }
 ];
